62f711255aac4d2578eb5d0611a0fffb61652beb
Denis Shadrin <deshaser@gmail.com>
2018-06-16 11:47:21 +0300

    create Area component, add scss support

---
+++ src/containers/area/area.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './area.css';
+
+
+class Area extends Component {
+  render() {
+    return (
+      <div className="area">
+        123
+      </div>
+    );
+  }
+}
+
+export default Area;
